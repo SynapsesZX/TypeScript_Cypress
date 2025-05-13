502ac90b3886648152d713074a1d144de73bba47
igorz <igorzyabrov2050@gmail.com>
2025-05-13 14:04:01 +0300

    Reworked the structure

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    specPattern: "cypress/e2e/**/*.cy.{jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       return config;
     },
