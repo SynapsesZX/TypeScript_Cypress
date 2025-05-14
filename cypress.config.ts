d638b9e0692666466b2da475acd07c88d64ce1ec
igorz <igorzyabrov2050@gmail.com>
2025-05-14 15:50:53 +0300

    added the github

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'ja53sg',
   e2e: {
     supportFile: "cypress/support/e2e.ts",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
